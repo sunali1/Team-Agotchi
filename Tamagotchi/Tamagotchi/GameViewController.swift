dbdaf9a2ce3bc04721e38207757ae038ec713caf
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-08 11:43:59 +0000

    Hooekd up to UI

--- Tamagotchi/Tamagotchi/GameViewController.swift
+++ Tamagotchi/Tamagotchi/GameViewController.swift
@@ -12,24 +12,65 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var creatureDisplay: UIImageView!
+    @IBOutlet weak var manualEggCrack: UIButton!
+    @IBOutlet weak var userInputField: UITextField!
+    @IBOutlet weak var userOutputField: UITextView!
+    
+    var egg = Egg()
+    
+    
+    
+    @IBAction func manualEggCrackAction(_ sender: Any) {
+        if creatureDisplay.image == UIImage(named: "eggcrack") {
+            creatureDisplay.image = UIImage(named: "photoegg3")
+            egg.healEggManual();
+            manualEggCrack.setTitle("Crack Da Egg", for: .normal)
+        }
+        else {
+            creatureDisplay.image = UIImage(named: "eggcrack")
+            egg.crackEggManual();
+            manualEggCrack.setTitle("Heal Da Egg", for: .normal)
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        userInputField.delegate = self;
+        
+        creatureDisplay.image = UIImage(named: "photoegg3")
+        manualEggCrack.setTitle("What happens when you press this?", for: .normal)
+        
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//            // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//            // Present the scene
+//                view.presentScene(scene)
+//            }
+//            
+//            view.ignoresSiblingOrder = true
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+        
+    }
+    
+    
+    @IBAction func textInputted(_ sender: Any) {
+        let helpedEgg = egg.helpEgg(item: userInputField.text!)
+        userOutputField.text = "\(helpedEgg)"
+    }
+    
+    
+    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        userInputField.resignFirstResponder()
     }
 
     override var shouldAutorotate: Bool {
@@ -50,6 +91,13 @@ class GameViewController: UIViewController {
     }
 
     override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension GameViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
         return true
     }
 }
