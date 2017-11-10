37a544172ed059511b6c90a67d2a014a82880acf
robincollins <racollins1@me.com>
2017-11-10 18:01:52 +0000

    comments on sleep stuff

--- Tamagotchi/Classes/CreatureClasses/Lion.swift
+++ Tamagotchi/Classes/CreatureClasses/Lion.swift
@@ -17,6 +17,7 @@ class Lion {
     var bursting: Bool
     var stomachContents: [String] = []
     var awake: Bool
+    let now = NSDate()
     
     init(size: Int, age: Int, temp: Int, hungry: Bool, bursting: Bool) {
         self.size = size;
@@ -43,12 +44,25 @@ class Lion {
         }
     }
     
-    func awakeStatus() -> String {
+    func timeTracker(time: Int) -> Int {
         
     }
     
+    func awakeStatus() -> String {
+        if awake == true {
+            return "waaaa i'm tired"
+            // take a time argument (based on actual time, and converted for our unit of time, and then let the player
+            // know that the lion needs to sleep, or
+            // does not need to sleep. depening on when it
+            // last had a sleep.
+            // depends on the age on the lion.
+            // perhaps use a hash, key day, value sleep or nil.
+        }
+    }
+    
     func sleep() {
-        // if time creature has been awake for 4 'hours', it needs to sleep
+        // if time creature has been awake for 4 'hours', it needs to sleep, and this makes it sleep.
+        // will sleep for a set time.
     }
     
 }
