770e0721f802d4c438708a2cf2a7b3cc5aaa42a9
robincollins <racollins1@me.com>
2017-11-10 15:59:36 +0000

    added eat and hunger status functions for the lion class

--- Tamagotchi/Classes/CreatureClasses/Lion.swift
+++ Tamagotchi/Classes/CreatureClasses/Lion.swift
@@ -13,11 +13,42 @@ class Lion {
     var size: Int
     var age: Int
     var temp: Int
+    var hungry: Bool
+    var bursting: Bool
+    var stomachContents: [String] = []
+    var awake: Bool
     
-    init() {
-        size = 0
-        age = 0
-        temp = 10
+    init(size: Int, age: Int, temp: Int, hungry: Bool, bursting: Bool) {
+        self.size = size;
+        self.age = age;
+        self.temp = temp;
+        self.hungry = true;
+        self.bursting = false;
+        self.awake = true;
     }
-
+    
+    
+    func eat(meal: String) -> String {
+       stomachContents.append(meal)
+        print(stomachContents)
+    }
+    
+    func hungerStatus() -> String {
+        if stomachContents.count <= 1 {
+            return "I am hungry!"
+        } else if stomachContents.count == 3 {
+            return "I am full!"
+        } else {
+            return ""
+        }
+    }
+    
+    func awakeStatus() -> String {
+        
+    }
+    
+    func sleep() {
+        // if time creature has been awake for 4 'hours', it needs to sleep
+    }
+    
 }
--- Tamagotchi/Classes/Manager/GameManager.swift
+++ Tamagotchi/Classes/Manager/GameManager.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class GameManager {
-    let egg = Egg()
-    let lion = Lion()
+    let egg = Egg(size: 0, age: 0, temp: 10, cracked: false)
+    let lion = Lion(size: 10, age: 6, temp: 15)
 }
