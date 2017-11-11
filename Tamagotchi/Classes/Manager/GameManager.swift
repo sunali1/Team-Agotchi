8f96d8aeaacaa6f54f6f134d58d126bd2ca579a0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-11 14:55:34 +0000

    Fixed build issues

--- Tamagotchi/Classes/Manager/GameManager.swift
+++ Tamagotchi/Classes/Manager/GameManager.swift
@@ -10,6 +10,6 @@ import Foundation
 
 class GameManager {
     let egg = Egg(size: 0, age: 0, temp: 10, cracked: false)
-    let lion = Lion(size: 10, age: 6, temp: 15)
+    let lion = Lion(size: 10, age: 6, temp: 15, hungry: true, bursting: false)
 
 }
