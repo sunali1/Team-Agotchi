690dff9bf19dce524a6d528ad66d3124581263a3
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-16 15:40:28 +0000

    Fixed santa position

--- Tamagotchi/Classes/CreatureClasses/Angel.swift
+++ Tamagotchi/Classes/CreatureClasses/Angel.swift
@@ -15,7 +15,7 @@ class Angel: SKSpriteNode {
         self.name = "angel"
         self.size = CGSize(width:200.0, height: 150.0)
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        self.position = CGPoint(x: -30, y: 470)
+        self.position = CGPoint(x: -30, y: 320)
         self.zPosition = 2;
         self.texture = SKTexture(imageNamed: "santa")
         print("found an angel")
